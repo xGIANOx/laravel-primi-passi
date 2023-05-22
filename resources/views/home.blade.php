1e7ab5bc92a0dcfa6cc0fb759cde1f299d244918
xGIANOx <studioenricogian@gmail.com>
2023-05-22 16:53:22 +0200

    [add] home page with Hello World

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel-primi-passi</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <a href="{{ route('home') }}">Home</a>
+        </header>
+        
+        <h2>{{ $page_title }}</h2>
+        <h1>{{$message}}</h1>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'page_title' => 'Home page',
+        'message' => 'Hello Laravel\'s World'
+    ];
+    return view('home', $data);
+})->name('home');
